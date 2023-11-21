e9a6bb0c8092b99303626ee5abf9fb29d278e26f
Emil Sebegatullin <emil.sebegatullin@infowatch.com>
2023-11-21 18:04:22 +0300

    = init.lua, lines.lua, theme.lua: change theme 'vscode'->'gruvbox_dark' ;

--- init.lua
+++ init.lua
@@ -1,13 +1,6 @@
--- if vim.g.vscode then
---     -- VSCode extension
--- else
-
 require("config.options")
 require("config.mappings")
 require("config.autocmds")
 require("config.lazy")
--- require("vscode").load()
 
-vim.cmd("colorscheme vscode")
-
--- end
+vim.cmd("colorscheme gruvbox")
