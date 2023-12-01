4c77610443fd6ac5afe9c62356db93a11c2322a1
Emil Sebegatullin <emil.sebegatullin@yandex.ru>
2023-12-01 21:48:21 +0300

    feat(ui): прозрачность окна

--- init.lua
+++ init.lua
@@ -4,3 +4,7 @@ require("config.autocmds")
 require("config.lazy")
 
 vim.cmd("colorscheme gruvbox")
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+vim.cmd("highlight Normal ctermbg=none")
+vim.cmd("highlight NonText ctermbg=none")
