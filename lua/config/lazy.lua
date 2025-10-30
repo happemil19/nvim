8c92385fb9ae60573b78204b28c16dd85617c22f
Emil Sebegatullin <emil.sebegatullin@infowatch.com>
2025-10-30 14:01:56 +0300

    = reborn conflicts after rebasing to start of history ;

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
--- install lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-   })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- load plugins
-require("lazy").setup("plugins")
-=======
 require("lazy").setup({
   -- Импортируем настройки плагинов
   spec = {
@@ -25,4 +7,3 @@ require("lazy").setup({
     { import = "plugins.tools" },
   },
 })
->>>>>>> b704291 (reborning ;)
