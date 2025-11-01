64cc76164e26ac017748973a47bc41f36fd30730
Emil Sebegatullin <emil.sebegatullin@infowatch.com>
2025-11-01 12:34:45 +0300

    * keymaps.lua: replace_text() ;

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,5 +28,59 @@ keymap("n", "<S-l>", "<cmd>bnext<CR>", { desc = "Next buffer" })
 keymap("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 keymap("n", "<leader>x", "<cmd>bd<CR>", { desc = "Close buffer" })
 
-keymap("v", "<C-r>", "<cmd>hy:%s/<c-r>h//gc<left><left><left><CR>", { desc = "Easier search and replace" })
 keymap("n", "<F3>", "<cmd>set hlsearch!", { desc = "clean last search highlighting" })
+
+-- Улучшенная замена текста с обработкой ошибок
+local function replace_text(confirm)
+  -- Проверяем, есть ли выделение
+  local mode = vim.fn.mode()
+  if mode ~= 'v' and mode ~= 'V' and mode ~= '' then
+    vim.notify("No text selected", vim.log.levels.WARN)
+    return
+  end
+
+  -- Сохраняем выделенный текст
+  vim.cmd('normal! "xy')
+  local selected = vim.fn.getreg('x')
+
+  if selected == '' or selected == nil then
+    vim.notify("No text selected", vim.log.levels.WARN)
+    return
+  end
+
+  -- Убираем лишние пробелы и переносы
+  selected = selected:gsub('^%s+', ''):gsub('%s+$', ''):gsub('\n', '\\n')
+
+  if selected == '' then
+    vim.notify("Selected text is empty", vim.log.levels.WARN)
+    return
+  end
+
+  -- Экранируем специальные символы для regex
+  local escaped = vim.fn.escape(selected, '/\\.*$^~[]()')
+
+  -- Запрашиваем замену
+  local replace_with = vim.fn.input('Replace "' .. selected .. '" with: ')
+
+  if replace_with == '' then
+    vim.notify("Replace cancelled", vim.log.levels.INFO)
+    return
+  end
+
+  -- Экранируем замену
+  local escaped_replace = vim.fn.escape(replace_with, '/\\')
+
+  -- Выполняем замену
+  local command = ':%s/' .. escaped .. '/' .. escaped_replace .. '/'
+  if confirm then
+    command = command .. 'gc'
+  else
+    command = command .. 'g'
+  end
+
+  vim.cmd(command)
+end
+
+-- Keymaps для замены
+keymap("v", "<leader>r", function() replace_text(true) end, { desc = "Replace selected text with confirmation" })
+keymap("v", "<leader>R", function() replace_text(false) end, { desc = "Replace selected text without confirmation" })
