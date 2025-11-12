c556cf7a26f1cae41d99812713f38fbcf820fb68
Emil Sebegatullin <emil.sebegatullin@infowatch.com>
2025-11-12 11:30:55 +0300

    * keymaps.lua, tools.lua: configured which-key ;

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -93,7 +93,19 @@ return {
     "numToStr/Comment.nvim",
     event = "BufReadPre",
     config = function()
-      require("Comment").setup()
+      require("Comment").setup({
+        padding = true,
+        sticky = true,
+        ignore = '^$',
+        -- toggler = { line = '<leader>cc', block = '<leader>bc' },
+        toggler = { line = 'gcc', block = 'gbc' },
+        -- opleader = { line = '<leader>c', block = '<leader>b' },
+        opleader = { line = 'gc', block = 'gb' },
+        extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+        mappings = { basic = true, extra = true },
+        pre_hook = nil,
+        post_hook = nil,
+      })
     end,
   },
 
@@ -193,4 +205,292 @@ return {
       })
     end,
   },
+
+  -- Which-key - подсказки по комбинациям клавиш
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      --@type false | "classic" | "modern" | "helix"
+      preset = "classic",
+      -- Delay before showing the popup. Can be a number or a function that returns a number.
+      ---@type number | fun(ctx: { keys: string, mode: string, plugin?: string }):number
+      delay = function(ctx)
+        return ctx.plugin and 0 or 1000
+      end,
+      filter = function(mapping)
+        -- example to exclude mappings without a description
+        -- return mapping.desc and mapping.desc ~= ""
+        return true
+      end,
+      --- You can add any mappings here, or use `require('which-key').add()` later
+      spec = {
+        --- Example
+        -- { "<leader>f", group = "file" }, -- group
+        -- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+        -- { "<leader>fb", function() print("hello") end, desc = "Foobar" },
+        -- { "<leader>fn", desc = "New File" },
+        -- { "<leader>f1", hidden = true }, -- hide this keymap
+        -- { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+        -- { "<leader>b", group = "buffers", expand = function()
+        --     return require("which-key.extras").expand.buf()
+        --   end
+        -- },
+        -- {
+        --   -- Nested mappings are allowed and can be added in any order
+        --   -- Most attributes can be inherited or overridden on any level
+        --   -- There's no limit to the depth of nesting
+        --   mode = { "n", "v" }, -- NORMAL and VISUAL mode
+        --   { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+        --   { "<leader>w", "<cmd>w<cr>", desc = "Write" },
+        -- }
+
+
+        { "<leader>a", group = " Actions" },
+        { "<leader>c", group = " Colors" },
+        { "<leader>d", group = " Debug" },
+        { "<leader>h", group = " Harpoon" },
+        { "<leader>n", group = " Notes" },
+        { "<leader>p", group = " Packer" },
+        { "<leader>r", group = " Replace" },
+        { "<leader>s", group = " Search" },
+        { "<leader>t", group = " Terminal" },
+        { "<leader>u", group = " UI" },
+        { "<leader>w", group = " Windows" },
+        { "<leader>x", group = " Trouble" },
+
+        { "<leader>b", group = " Buffers" },
+        { "<leader>bd", "<cmd>bd<CR>", desc = "Close buffer" },
+        { "<leader>bn", "<cmd>bnext<CR>", desc = "Next buffer" },
+        { "<leader>bp", "<cmd>bprevious<CR>", desc = "Previous buffer" },
+
+        { "<leader>e", group = " Explorer" },
+        { "<leader>ee", vim.cmd.Ex, desc = "File explorer" },
+        { "<leader>en", "<cmd>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+
+        { "<leader>q", group = " Quit" },
+        { "<leader>qq", "<cmd>q<CR>", desc = "Quit" },
+        { "<leader>qs", "<cmd>wq<CR>", desc = "Save and quit" },
+        { "<leader>qd", "<cmd>q!<CR>", desc = "Force quit" },
+
+        { "<leader>l", group = " LSP" },
+        { "<leader>lr", vim.lsp.buf.rename, desc = "Rename" },
+        { "<leader>la", vim.lsp.buf.code_action, desc = "Code action" },
+
+        { "<leader>f", group = "File" },
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+        { "<leader>fw", "<cmd>w<cr>", desc = "Save File" },
+
+        { "<leader>g", group = " Git" },
+        { "<leader>gg", "<cmd>LazyGit<CR>", desc = "LazyGit" },
+        { "<leader>gc", "<cmd>Git commit<cr>", desc = "Commit" },
+        { "<leader>gp", "<cmd>Git push<cr>", desc = "Push" },
+        { "<leader>gP", "<cmd>Git pull<CR>", desc = "Pull" },
+        { "<leader>gs", "<cmd>Git status<cr>", desc = "Status" },
+        { "<leader>gd", "<cmd>Gdiffsplit<CR>", desc = "Diff" },
+        -- { "<leader>gb", "<cmd>Git blame<CR>", desc = "Blame" },
+        { "<leader>gj", function() require('gitsigns').next_hunk() end, desc = "Next hunk" },
+        { "<leader>gk", function() require('gitsigns').prev_hunk() end, desc = "Prev hunk" },
+
+        -- { "[d", vim.diagnostic.goto_prev, desc = "Prev diagnostic" },  -- Deprecated.
+        -- { "]d", vim.diagnostic.goto_next, desc = "Next diagnostic" },  -- Deprecated.
+
+        -- LSP goto
+        { "gD", vim.lsp.buf.declaration, desc = "Declaration" },
+        { "gd", vim.lsp.buf.definition, desc = "Definition" },
+        { "gi", vim.lsp.buf.implementation, desc = "Implementation" },
+        { "gr", vim.lsp.buf.references, desc = "References" },
+        { "K", vim.lsp.buf.hover, desc = "Hover" },
+
+        {
+          mode = { "v" },  -- VISUAL mode
+          { "<leader>r", function()
+              -- Простая версия замены текста
+              vim.cmd('normal! "xy')
+              local selected = vim.fn.getreg('x')
+              if selected ~= '' then
+                local escaped = vim.fn.escape(selected, '/\\')
+                local replace_with = vim.fn.input('Replace "' .. selected .. '" with: ')
+                if replace_with ~= '' then
+                  vim.cmd(':%s/' .. escaped .. '/' .. replace_with .. '/gc')
+                end
+              end
+            end, desc = "Replace selected text" },
+          { "<leader>R", function()
+              -- Функция замены без подтверждения
+              vim.cmd('normal! "xy')
+              local selected = vim.fn.getreg('x')
+              if selected ~= '' then
+                local escaped = vim.fn.escape(selected, '/\\')
+                local replace_with = vim.fn.input('Replace ALL "' .. selected .. '" with: ')
+                if replace_with ~= '' then
+                  vim.cmd(':%s/' .. escaped .. '/' .. replace_with .. '/g')
+                end
+              end
+            end, desc = "Replace selected text without confirmation" },
+        },
+      },
+      -- show a warning when issues were detected with your mappings
+      notify = true,
+      -- Which-key automatically sets up triggers for your mappings.
+      -- But you can disable this and setup the triggers manually.
+      -- Check the docs for more info.
+      triggers = {
+        { "<auto>", mode = "nxso" },  -- { "<leader>", mode = { "n", "v" } },
+      },
+
+      -- triggers_blacklist = {
+      --   -- список режимов, где which-key не показывается
+      --   i = { "j", "k" },
+      --   v = { "j", "k" },
+      -- },
+
+      -- Start hidden and wait for a key to be pressed before showing the popup
+      -- Only used by enabled xo mapping modes.
+      ---@param ctx { mode: string, operator: string }
+      defer = function(ctx)
+        return ctx.mode == "V" or ctx.mode == "<C-V>"
+      end,
+      plugins = {
+        marks = true, -- shows a list of your marks on ' and `
+        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+        -- No actual key bindings are created
+        spelling = {
+          enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+          suggestions = 20, -- how many suggestions should be shown in the list?
+        },
+        presets = {
+          operators = true, -- adds help for operators like d, y, ...
+          motions = true, -- adds help for motions
+          text_objects = true, -- help for text objects triggered after entering an operator
+          windows = true, -- default bindings on <c-w>
+          nav = true, -- misc bindings to work with windows
+          z = true, -- bindings for folds, spelling and others prefixed with z
+          g = true, -- bindings for prefixed with g
+        },
+      },
+      win = {
+        -- don't allow the popup to overlap with the cursor
+        no_overlap = true,
+        -- width = 1,
+        -- height = { min = 4, max = 25 },
+        -- col = 0,
+        -- row = math.huge,
+        -- border = "none",
+        padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+        title = true,
+        title_pos = "center",
+        zindex = 1000,
+        -- Additional vim.wo and vim.bo options
+        bo = {},
+        wo = {
+          -- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+        },
+      },
+      layout = {
+        height = { min = 4, max = 25 }, -- min and max width of the window
+        width = { min = 20 }, -- min and max width of the columns
+        spacing = 3, -- spacing between columns
+        align = "left", -- left, center, right
+      },
+      keys = {
+        scroll_down = "<c-d>", -- binding to scroll down inside the popup
+        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+      },
+      --- Mappings are sorted using configured sorters and natural sort of the keys
+      --- Available sorters:
+      --- * local: buffer-local mappings first
+      --- * order: order of the items (Used by plugins like marks / registers)
+      --- * group: groups last
+      --- * alphanum: alpha-numerical first
+      --- * mod: special modifier keys last
+      --- * manual: the order the mappings were added
+      --- * case: lower-case first
+      sort = { "local", "order", "group", "alphanum", "mod" },
+      ---@type number|fun(node):boolean?
+      expand = 0, -- expand groups when <= n mappings
+      -- expand = function(node)
+      --   return not node.desc -- expand all nodes without a description
+      -- end,
+      -- Functions/Lua Patterns for formatting the labels
+      ---@type table<string, ({[1]:string, [2]:string}|fun(str:string):string)[]>
+      replace = {
+        key = {
+          function(key)
+            return require("which-key.view").format(key)
+          end,
+          -- { "<Space>", "SPC" },
+        },
+        desc = {
+          { "<Plug>%(?(.*)%)?", "%1" },
+          { "^%+", "" },
+          { "<[cC]md>", "" },
+          { "<[cC][rR]>", "" },
+          { "<[sS]ilent>", "" },
+          { "^lua%s+", "" },
+          { "^call%s+", "" },
+          { "^:%s*", "" },
+        },
+      },
+      icons = {
+        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+        separator = "➜", -- symbol used between a key and it's label
+        group = "+", -- symbol prepended to a group
+        ellipsis = "…",
+        -- set to false to disable all mapping icons,
+        -- both those explicitly added in a mapping
+        -- and those from rules
+        mappings = true,
+        --- See `lua/which-key/icons.lua` for more details
+        --- Set to `false` to disable keymap icons from rules
+        rules = {},
+        -- use the highlights from mini.icons
+        -- When `false`, it will use `WhichKeyIcon` instead
+        colors = true,
+        -- used by key format
+        keys = {
+          Up = " ",
+          Down = " ",
+          Left = " ",
+          Right = " ",
+          C = "󰘴 ",
+          M = "󰘵 ",
+          D = "󰘳 ",
+          S = "󰘶 ",
+          CR = "󰌑 ",
+          Esc = "󱊷 ",
+          ScrollWheelDown = "󱕐 ",
+          ScrollWheelUp = "󱕑 ",
+          NL = "󰌑 ",
+          BS = "󰁮",
+          Space = "󱁐 ",
+          Tab = "󰌒 ",
+          F1 = "󱊫",
+          F2 = "󱊬",
+          F3 = "󱊭",
+          F4 = "󱊮",
+          F5 = "󱊯",
+          F6 = "󱊰",
+          F7 = "󱊱",
+          F8 = "󱊲",
+          F9 = "󱊳",
+          F10 = "󱊴",
+          F11 = "󱊵",
+          F12 = "󱊶",
+        },
+      },
+      show_help = true, -- show a help message in the command line for using WhichKey
+      show_keys = true, -- show the currently pressed key and its label as a message in the command line
+      -- disable WhichKey for certain buf types and file types.
+      disable = {
+        ft = { "TelescopePrompt" },
+        bt = { "nofile", "prompt" },
+      },
+      debug = false, -- enable wk.log in the current directory
+    },
+  }
 }
